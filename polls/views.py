f74f404f30883d4e540c8ae9dcddb10689d80622
Dillon Harless <dillon@cloudfruit.com>
2024-01-11 18:43:25 -0500

    converted my views to generic views

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,29 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.db.models import F
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+
 from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
@@ -34,8 +41,13 @@ def vote(request, question_id):
             },
         )
     else:
-        selected_choice.votes += 1
+        # This is probably the simplest way to use F() to avoid race conditions
+        # The database handles the updates entirely with this approach
+        selected_choice.votes = F("votes") + 1
         selected_choice.save()
+        # This ensures we get the updated value since Python never sees the original
+        selected_choice.refresh_from_db() 
+
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
